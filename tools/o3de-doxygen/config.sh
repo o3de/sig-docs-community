ccf203d2ebe9eb4cdeb3d1a681a62ac77a2818f2
chanmosq <75444793+chanmosq@users.noreply.github.com>
2023-04-20 13:47:13 -0700

    o3de-doxygen tool - fix issue with Gem APIs not generating, lessen setup steps.
    Signed-off-by: chanmosq <75444793+chanmosq@users.noreply.github.com>

--- tools/o3de-doxygen/config.sh
+++ tools/o3de-doxygen/config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Path to user's local o3de repository  (e.g. $HOME/o3de)
+# Path to user's local o3de repository  (e.g. =$HOME/o3de)
 O3DE_PATH=
 
-# Path to user's local o3de.org repository (e.g. $HOME/o3de.org)
+# Path to user's local o3de.org repository (e.g. =$HOME/o3de.org)
 O3DEORG_PATH=
 
-# O3DE version number, passed to Doxygen configuration
+# O3DE version number, passed to Doxygen configuration (e.g. =23.05.0)
 PROJECT_NUMBER=
